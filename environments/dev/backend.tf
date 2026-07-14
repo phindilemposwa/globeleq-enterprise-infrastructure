cde976a30189f78240a33534e679ea40b9dc2195
Phindile Mposwa <pamposwa@shoprite.co.za>
2026-07-14 20:42:49 +0200

    fixed backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "globeleq-terraform-state-574548986680"
-    key          = "dev/terraform.tfstate"
-    region       = "eu-west-1"
-    encrypt      = true
-    use_lockfile = true
+    bucket  = "globeleq-terraform-state-574548986680"
+    key     = "dev/terraform.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
   }
 }
